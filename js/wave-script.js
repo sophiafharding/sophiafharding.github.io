1fcd912bf8ee05c53f568f140a5140fbda0bb2eb
joeyShea <142829755+sophiafharding@users.noreply.github.com>
2024-10-24 16:54:13 -0500

    Tweaks

--- js/wave-script.js
+++ js/wave-script.js
@@ -35,7 +35,7 @@ function drawWave() {
   ctx.lineTo(0, canvas.height); // Left bottom corner
   ctx.closePath();
 
-  ctx.fillStyle = "#efc3ca";
+  ctx.fillStyle = "#e6acb6";
   ctx.fill();
 
   // Move the wave to the left
